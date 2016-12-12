779baa16069ec7a62852ff0a4d8cc9148b9e1179
Konstantin Zhitkov <zhitkovk@users.noreply.github.com>
2016-12-12 18:16:30 +0400

    Update clValid-functions.R

--- R/clValid-functions.R
+++ R/clValid-functions.R
@@ -31,7 +31,7 @@ clValid <- function(obj, nClust, clMethods="hierarchical", validation="stability
 
   validation <- match.arg(validation,c("stability","internal","biological"),several.ok=TRUE)
   metric <- match.arg(metric,c("euclidean", "correlation", "manhattan")) ## used for hierarchical, diana, fanny, agnes, pam
-  method <- match.arg(method,c("ward", "single", "complete", "average")) ## for hclust, agnes
+  method <- match.arg(method,c("ward.D", "ward.D2", "single", "complete", "average")) ## for hclust, agnes
   GOcategory <- match.arg(GOcategory, c("all","BP","CC","MF"))
 
   switch(class(obj),
